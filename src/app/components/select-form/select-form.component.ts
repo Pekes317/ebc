8c9674d39f1d0c7389bd08bae4312e43afbda775
Pekes317 <el_dante565@yahoo.com>
2019-01-05 22:49:16 -0600

    more form stuff

--- src/app/components/select-form/select-form.component.ts
+++ src/app/components/select-form/select-form.component.ts
@@ -1,12 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { Item } from '../../state/item-store/models/item.model';
+import { ItemType } from '../../util/item-type.enum';
 
 @Component({
   selector: 'ebc-select-form',
   templateUrl: './select-form.component.html',
-  styleUrls: ['./select-form.component.scss']
+  styleUrls: ['./select-form.component.scss'],
 })
 export class SelectFormComponent implements OnInit {
+  itemType: ItemType = ItemType.card;
+  previewImg: string;
+  selectForm: FormGroup;
+  selectType: FormControl = new FormControl('', Validators.required);
+  templates: Array<Item> = [];
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.selectForm = new FormGroup({
+      selectType: this.selectType,
+    });
+  }
+
+  goBack() {}
+
+  setPreview(pic: Event) {}
+
+  submitTemp() {}
 }
